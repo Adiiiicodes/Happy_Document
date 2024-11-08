25859bd37ee44412cff34e71cc12b4cb7d243e74
Aditya Nalawade <157732130+Adiiiicodes@users.noreply.github.com>
2024-11-09 01:56:47 +0530

    image.py

--- HAPPY_DOCUMENT/image.py
+++ HAPPY_DOCUMENT/image.py
@@ -4,6 +4,11 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from PIL import Image, ImageTk
 import os
+import webbrowser
+import tkinter as tk
+from tkinter import ttk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
 class ImageCropper:
     def __init__(self, master, image_path):
@@ -63,6 +68,10 @@ class ImageCropper:
         self.canvas.bind("<Button-5>", self.on_mousewheel_down)  # Linux
         self.canvas.bind("<Shift-Button-4>", self.on_mousewheel_left)  # Linux with Shift
         self.canvas.bind("<Shift-Button-5>", self.on_mousewheel_right)  # Linux with Shift
+
+
+    
+
 
     def on_mousewheel_y(self, event):
         # Windows mouse wheel scrolling (vertical)
@@ -201,6 +210,37 @@ class ImageUtilityGUI:
         self.status_bar.pack(fill=X, side=BOTTOM, pady=15)
 
         self.selected_files = []
+
+
+    # Footer
+        self.footer_frame = ttk.Frame(master)
+        self.footer_frame.pack(side=BOTTOM, fill=X, pady=(10, 0))
+
+        footer_label = ttk.Label(self.footer_frame, text="Created By : ", font=("Helvetica", 12))
+        footer_label.pack(side=LEFT, padx=5)
+
+        link_label = ttk.Label(self.footer_frame, text="Aditya Nalawade", font=("Helvetica", 12, "underline"), foreground="#CBFC01")
+        link_label.pack(side=LEFT)
+
+        link_label2 = ttk.Label(self.footer_frame, text="GitHub", font=("Helvetica", 12, "underline"), foreground="#CBFC01")
+        link_label2.pack(side=LEFT, padx=5)
+        link_label2.bind("<Button-1>", self.open_link2)
+
+        link_label3 = ttk.Label(self.footer_frame, text="Mail", font=("Helvetica", 12, "underline"), foreground="#CBFC01")
+        link_label3.pack(side=LEFT, padx=5)
+        link_label3.bind("<Button-1>", self.open_link3)
+
+        link_label.bind("<Button-1>", self.open_link)
+
+    def open_link(self, event):
+        webbrowser.open("https://www.linkedin.com/in/aditya-nalawade-a4b081297?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app")
+
+    def open_link2(self, event):
+        webbrowser.open("https://github.com/Adiiiicodes")
+
+    def open_link3(self, event):
+        webbrowser.open("adityacodes8@gmail.com")
+
 
     def browse_files(self):
         files = filedialog.askopenfilenames(filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp;*.gif")])
